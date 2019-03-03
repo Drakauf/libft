4979f0588f59f1f0821562f8bef0c5d089505edf
Drakauf <shthevak@z2r1p1.le-101.fr>
2019-03-03 15:30:57 +0100

    added : ft_atoi [Norme, include, makefile, man_fr, man_en]

--- includes/ft_int.h
+++ includes/ft_int.h
@@ -1,36 +1,19 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_atoi.c                                        .::    .:/ .      .::   */
+/*   ft_int.h                                         .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: shthevak <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/03/03 10:49:37 by shthevak     #+#   ##    ##    #+#       */
-/*   Updated: 2019/03/03 15:28:53 by shthevak    ###    #+. /#+    ###.fr     */
+/*   Created: 2019/03/03 15:18:39 by shthevak     #+#   ##    ##    #+#       */
+/*   Updated: 2019/03/03 15:28:56 by shthevak    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-int		ft_atoi(const char *str)
-{
-	unsigned long	r;
-	int				s;
+#ifndef FT_INT_H
+# define FT_INT_H
 
-	s = 1;
-	r = 0;
-	while ((*str == '\t' || *str == '\f' || *str == '\v' ||
-				*str == '\n' || *str == '\r' || *str == ' ')
-			&& *str != '\0')
-		str++;
-	if (*str == '-' || *str == '+')
-		if (*str++ == '-')
-			s = -1;
-	while (*str >= '0' && *str <= '9')
-	{
-		r = r * 10 + *str - 48;
-		str++;
-	}
-	if (r >= 9223372036854775807)
-		return (s == 1 ? -1 : 0);
-	return (s * (int)r);
-}
+int		ft_atoi(const char *str);
+
+#endif

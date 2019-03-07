c4d9dfcb22f0e6143db09e97a28d3b24c34064eb
Drakauf <shthevak@z2r1p1.le-101.fr>
2019-03-07 13:43:01 +0100

    [str/] added : ft_strjoin && ft_strjoinfree [Norme, include, makefile, man_fr, man_en] Other modified

--- includes/ft_str.h
+++ includes/ft_str.h
@@ -6,7 +6,7 @@
 /*   By: shthevak <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/03/03 15:18:39 by shthevak     #+#   ##    ##    #+#       */
-/*   Updated: 2019/03/07 13:31:58 by shthevak    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/03/07 13:41:28 by shthevak    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -30,9 +30,11 @@ void	ft_putstr(const char *s);
 void	ft_putstr_fd(const char *s, int fd);
 void	ft_putstr_tab(char **tab);
 char	*ft_strcat(char *dest, char *src);
-int		ft_strcmp(cont char *s1, const char *s2);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dest, const char *str);
 void	ft_strdel(char **str);
 char	*ft_strdup(char *str);
-int		ft_strequ(cont char *s1, const char *s2);
+int		ft_strequ(const char *s1, const char *s2);
+char	*ft_strjoin(const char *s1, const char *s2);
+void	ft_strjoinfree(char **s1, const char *s2);
 #endif

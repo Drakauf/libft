718b8201738f7786840c2ba7831833f832bf93c1
Drakauf <shthevak@z2r1p1.le-101.fr>
2019-03-07 11:23:49 +0100

    [str/] added : ft_strcat [Norme, include, makefile, man_fr, man_en] Other modified

--- includes/ft_str.h
+++ includes/ft_str.h
@@ -6,7 +6,7 @@
 /*   By: shthevak <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/03/03 15:18:39 by shthevak     #+#   ##    ##    #+#       */
-/*   Updated: 2019/03/05 14:12:20 by shthevak    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/03/07 11:23:01 by shthevak    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -29,4 +29,5 @@ void	ft_putendl_fd(const char *s, int fd);
 void	ft_putstr(const char *s);
 void	ft_putstr_fd(const char *s, int fd);
 void	ft_putstr_tab(char **tab);
+char	*ft_strcat(char *dest, char *src);
 #endif

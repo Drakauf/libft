24d99f3bae7a5040cc23e0d46378cf1947a0f376
Drakauf <shthevak@z2r1p1.le-101.fr>
2019-03-17 16:13:35 +0100

    Normed

--- src/unix/ft_create_file.c
+++ src/unix/ft_create_file.c
@@ -6,7 +6,7 @@
 /*   By: shthevak <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/03/15 14:57:40 by shthevak     #+#   ##    ##    #+#       */
-/*   Updated: 2019/03/15 15:41:15 by shthevak    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/03/17 16:12:56 by shthevak    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,6 +16,7 @@
 int		ft_create_file(char *path)
 {
 	char *(tab[3]);
+
 	if (access(path, F_OK) != 0)
 	{
 		if (access("/usr/bin/touch", X_OK) == 0)
@@ -23,7 +24,7 @@ int		ft_create_file(char *path)
 			tab[0] = "/usr/bin/touch";
 			tab[1] = path;
 			tab[2] = NULL;
-			if(ft_execute("/usr/bin/touch", tab, NULL))
+			if (ft_execute("/usr/bin/touch", tab, NULL))
 				return (1);
 			return (0);
 		}
